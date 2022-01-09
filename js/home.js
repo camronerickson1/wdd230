8e7103b8d6455c81a829f05af379b5d40905879a
camronerickson1 <eri20010@byui.edu>
2022-01-08 19:47:47 -0700

    last modified fixed

--- js/home.js
+++ js/home.js
@@ -1,50 +1,9 @@
-const daynames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
 const d = new Date();
-const dayName = daynames[d.getDay()];
-const monthName = months[d.getMonth()];
 const year = d.getFullYear();
-const hour = d.getHours();
-const minute = d.getMinutes();
-const second = d.getSeconds();
 
+let update = document.lastModified;
+document.getElementById("currentdate").innerHTML = update;
 
-const date2 = document.querySelector('#currentdate');
-
-
-
-try {
-    const options = {day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'};
-    date2.textContent = new Date().toLocaleDateString('en-US', options);
-  } catch (e) {
-    alert('Error with code or your browser does not support Locale');
-  }
 
 const fullYear = document.querySelector('#year');
-
-try {
-    fullYear.textContent = year;
-} catch (e) {
-    alert('Error with code or your browser does not support Locale');
-}
+fullYear.textContent = year;
