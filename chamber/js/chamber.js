086e9d922f8bf1532a3756faf8ceb6ed551bc3b2
camronerickson1 <eri20010@byui.edu>
2022-01-27 19:12:04 -0700

    Starting with different viewports

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -14,3 +14,11 @@ try {
 } catch (e) {
     alert('Error with code or your browser does not support Locale');
 }
+
+function toggleMenu() {
+    document.getElementById("main-nav").classList.toggle("open");
+    document.getElementById("hamburger-btn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburger-btn");
+x.onclick = toggleMenu;
