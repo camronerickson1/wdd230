8f2fb275688aa3d7bdd9a8adec1dacf0add92bc9
camronerickson1 <eri20010@byui.edu>
2022-02-19 22:47:59 -0700

    fixed last visit js

--- chamber/js/lastVisit.js
+++ chamber/js/lastVisit.js
@@ -3,7 +3,7 @@ const visitsDisplay = document.getElementById('visitInfo');
 let lastVisit = Number(window.localStorage.getItem('visits-ls'));
 
 if (lastVisit == 0) {
-    visitsDisplay.textContent = `This is your first visit!`;
+    visitsDisplay.textContent = `Today!`;
 } else {
     let dateDifference = Math.round((Date.now() - lastVisit) / (60*60*24*1000)); 
     visitsDisplay.textContent = `${dateDifference} days ago`;
