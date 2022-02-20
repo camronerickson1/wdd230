ed33f13c0c47e58fb73aa8d07db48e7a6b7c1f90
camronerickson1 <eri20010@byui.edu>
2022-02-19 23:05:19 -0700

    fixed if statement

--- chamber/js/lastVisit.js
+++ chamber/js/lastVisit.js
@@ -2,7 +2,7 @@ const visitsDisplay = document.getElementById('visitInfo');
 
 let lastVisit = Number(window.localStorage.getItem('visits-ls'));
 
-if (lastVisit == 0) {
+if (Math.round((Date.now() - lastVisit) / (60*60*24*1000)) == 0) {
     visitsDisplay.textContent = `Today!`;
 } else {
     let dateDifference = Math.round((Date.now() - lastVisit) / (60*60*24*1000)); 
