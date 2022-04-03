1d1fa98034e055bd43ca173d4afddcb6718dc814
camronerickson1 <eri20010@byui.edu>
2022-04-03 17:07:38 -0600

    done with home small view

--- final/js/temple.js
+++ final/js/temple.js
@@ -5,3 +5,6 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburger-btn");
 x.onclick = toggleMenu;
+
+let update = document.lastModified;
+document.getElementById("last-modified").innerHTML = update;
