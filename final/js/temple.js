083d14b89e91e667e97719de9e8fdc84a2aa70d9
camronerickson1 <eri20010@byui.edu>
2022-04-03 16:37:30 -0600

    added style to home

---
+++ final/js/temple.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    document.getElementById("main-nav").classList.toggle("open");
+    document.getElementById("hamburger-btn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburger-btn");
+x.onclick = toggleMenu;
