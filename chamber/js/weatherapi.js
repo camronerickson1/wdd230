7bf334c298b061687d0402c9a57b1da8e827ad24
camronerickson1 <eri20010@byui.edu>
2022-04-05 18:25:15 -0600

    added weather and windchill info

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -3,19 +3,32 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5781860&units
 fetch(apiURL)
     .then((response) => response.json())
     .then((x) => {
-        // console.log(jsObject);
+        console.log(x);
         const currentTemp = document.querySelector("#current-temp");
         const weathericon = document.querySelector("#weathericon");
         const caption = document.querySelector('figcaption');
+        const windSpeed = document.querySelector('#windspeed');
 
         currentTemp.textContent = x.main.temp.toFixed(0);
         let imgsrc = `https://openweathermap.org/img/w/${x.weather[0].icon}.png`;
         let imgalt = x.weather[0].description;
         imgalt = imgalt.split(' ').map(capitalize).join(' ');
+        let wind = x.wind.speed;
 
         weathericon.setAttribute('src', imgsrc);
         weathericon.setAttribute('alt', imgalt);
         caption.textContent = imgalt;
+        windSpeed.textContent = wind;
+
+        const temp = x.main.temp.toFixed(0);
+        const speed = x.wind.speed;
+        if(temp <= 50 && speed > 3) {
+            const windchill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 * temp * Math.pow(speed,0.16);
+            document.querySelector("#wind-chill").innerHTML = `${Math.round(windchill)}&#176;F`;
+        }
+        else {
+            document.querySelector("#wind-chill").innerHTML = "N/A"
+        }
     });
 
     function capitalize(word) {
